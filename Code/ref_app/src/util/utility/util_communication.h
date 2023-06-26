d2a020081f7a08c235b413ff7a475be2a8bfc5fc
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-06-26 14:04:07 +0200

    Communication goes header-only and up to 8MHz

--- Code/ref_app/src/util/utility/util_communication.h
+++ Code/ref_app/src/util/utility/util_communication.h
@@ -17,30 +17,48 @@
 
   namespace util
   {
+    namespace communication_buffer_depth_one_byte
+    {
+      using buffer_value_type = std::uint8_t;
+    } // namespace communication_buffer_depth_one_byte
+
     template<typename CommunicationEngineType>
     class communication final : private util::noncopyable
     {
     public:
       using communication_engine_type = CommunicationEngineType;
 
-      auto recv(std::uint8_t& byte_to_recv) -> bool { return my_com_engine.recv(byte_to_recv); }
+      static auto recv() -> communication_buffer_depth_one_byte::buffer_value_type { return my_com_engine.recv(); }
 
       static auto   select() -> void { communication_engine_type::select(); }
       static auto deselect() -> void { communication_engine_type::deselect(); }
 
+      static auto send(const std::uint8_t byte_to_send) -> bool { return my_com_engine.send(byte_to_send); }
+
       template<typename SendIteratorType>
-      auto send_n(SendIteratorType first, SendIteratorType last) -> bool { return my_com_engine.send_n(first, last); }
+      static auto send_n(SendIteratorType first, SendIteratorType last) -> bool
+      {
+        auto result_send_is_ok = true;
 
-      auto send(const std::uint8_t byte_to_send) -> bool { return my_com_engine.send(byte_to_send); }
+        while(first != last)
+        {
+          result_send_is_ok = (my_com_engine.send(static_cast<std::uint8_t>(*first++)) && result_send_is_ok);
+        }
 
-    protected:
-      communication_engine_type my_com_engine { };
+        return result_send_is_ok;
+      }
+
+      static auto get() -> communication_engine_type& { return my_com_engine; }
+
+    private:
+      static communication_engine_type my_com_engine;
     };
 
-    namespace communication_buffer_depth_one_byte
-    {
-      using buffer_value_type = std::uint8_t;
-    } // namespace communication_buffer_depth_one_byte
+    template<typename CommunicationEngineType>
+    typename communication<CommunicationEngineType>::communication_engine_type communication<CommunicationEngineType>::my_com_engine;
+
+    template<typename CommunicationEngineType>
+    using communication_type = communication<CommunicationEngineType>;
   }
 
 #endif // UTIL_COMMUNICATION_2012_05_31_H
